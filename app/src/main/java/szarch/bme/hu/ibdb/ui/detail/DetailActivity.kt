1dcc1700d9818f6726ada297b1f48977597f0918
Taki1994 <taki94norbi@gmail.com>
2018-11-12 23:49:06 +0100

    Layout changes, business logic implementations, Glide-Picasso changing due to Glide issues

--- app/src/main/java/szarch/bme/hu/ibdb/ui/detail/DetailActivity.kt
+++ app/src/main/java/szarch/bme/hu/ibdb/ui/detail/DetailActivity.kt
@@ -2,10 +2,13 @@ package szarch.bme.hu.ibdb.ui.detail
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_detail.*
 import kotlinx.android.synthetic.main.content_detail.*
 import szarch.bme.hu.ibdb.R
 import szarch.bme.hu.ibdb.network.models.book.BookResponse
 import szarch.bme.hu.ibdb.ui.base.BaseApplication
+import szarch.bme.hu.ibdb.util.StringUtil
 
 class DetailActivity : AppCompatActivity(), DetailScreen {
 
@@ -24,13 +27,27 @@ class DetailActivity : AppCompatActivity(), DetailScreen {
 
     override fun showBookDetail(bookResponse: BookResponse) {
         bookResponse.imageUrl?.let {
-            /*GlideApp.with(this)
+            Picasso.get()
                 .load(it)
-                .into(iv_detail_book_image_url)*/
-
+                .fit()
+                .into(iv_detail)
         }
 
         tv_detail_book_title.text = bookResponse.title
+        tv_detail_book_author.text = bookResponse.author
+        tv_detail_book_publisher.text = bookResponse.publisher
+        bookResponse.published?.let {
+            tv_detail_book_published.text = StringUtil.formatDateToString(it)
+        }
+        tv_detail_book_summary.text = bookResponse.summary
+        tv_detail_book_page_number.text = bookResponse.pageNumber.toString()
+        bookResponse.sold?.let {
+            tv_detail_book_sold.text = it.toString()
+        }
+        bookResponse.views?.let {
+            tv_detail_book_views.text = it.toString()
+        }
+
 
     }
 
